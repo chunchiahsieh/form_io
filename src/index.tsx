3bf67147e376dfeb56e73e26cccb8a92b4a93efc
chunchiahsieh <chunchiahsieh2@gmail.com>
2025-03-04 16:41:30 +0800

    安裝form.io多國語言測試中

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,47 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MyForm } from "./components/MyForm"; // 直接載入 MyForm
+import i18next from "i18next";
+import { Formio } from "@formio/js";
+
+// 初始化 i18next
+i18next.init({
+  lng: "zh", // 預設語言：中文
+  fallbackLng: "en",
+  resources: {
+    zh: {
+      translation: {  // 注意這一層必須存在
+        "Search field(s)": "搜尋欄位",
+        "Text Field": "文字欄位",
+        "Number": "數字",
+        "Password": "密碼",
+        "Checkbox": "核取方塊",
+        "Select Boxes": "多選方塊",
+        "Select": "選擇",
+        "Radio": "單選",
+        "Button": "按鈕",
+        "Submit": "提交",
+        "Save": "儲存",
+        "Cancel": "取消",
+        "Edit": "編輯",
+        "Delete": "刪除"
+      }
+    }
+  },
+});
+(Formio as any).i18next = i18next;
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <MyForm />
   </React.StrictMode>
 );
 
